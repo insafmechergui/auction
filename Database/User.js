af08a356fbedd201b8a0cbcff43716b22bb2cd4a
waj949 <wajdihamrouni1993@gmail.com>
2020-01-05 16:23:51 +0100

    user schema

--- Database/User.js
+++ Database/User.js
@@ -3,8 +3,9 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  // name: String,
-  // description: String
+  name: String,
+  email: { type: String, unique: true },
+  password: String
 });
 
 mongoose.model("user", userSchema);
