5b6245a2f16399784b15f9d8df28f1b41ddf00c9
waj949 <wajdihamrouni1993@gmail.com>
2020-01-05 14:55:10 +0100

    init

---
+++ client/src/App.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import About from "./components/about";
+import Users from "./components/users";
+import "./App.css";
+
+import productService from "./services/productService";
+
+function App() {
+  // const getProducts = async () => { // exemple for using axios from servesie
+  //   let res = await productService.getAll();
+  //   console.log(res);
+  //   setproducts(res);
+  // };
+
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ client/src/components/about.js
@@ -0,0 +1,10 @@
+import React from "react";
+class About extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return <div>about </div>;
+  }
+}
+export default About;
