1eca2e4810a3d004b6a8219e1b5ffbf977be7374
Mohamed-Dhia <dhiaweslati16@gmail.com>
2020-01-09 11:06:44 +0100

    gruuunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,35 +1,38 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     shell: {
-      gitAdd: {
-        commmand: "git add ."
+      ls: {
+        command: "ls"
       },
-      gitcommit: {
-        commmand: msg =>
-          `git commit -m \"commited by the grunt gang with the msg ${msg}\"`
+      add: {
+        command: "git add ."
       },
-      gitPush: {
-        commmand: "git push origin master"
+      commit: {
+        command: 'git commit -m "commited by grunt gang"'
+      },
+      push: {
+        command: "git push origin master"
       }
-    },
-
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        eqnull: true,
-        browser: true,
-        globals: {
-          jQuery: true
-        },
-        esversion: 6
-      },
-      all: ["Gruntfile.js", "client", "routes"]
     }
+
+    // jshint: {
+    //   options: {
+    //     curly: true,
+    //     eqeqeq: true,
+    //     eqnull: true,
+    //     browser: true,
+    //     globals: {
+    //       jQuery: true
+    //     },
+    //     esversion: 6
+    //   },
+    //   all: ["Gruntfile.js", "client", "routes"]
+    // }
   });
   grunt.loadNpmTasks("grunt-contrib-jshint");
   grunt.loadNpmTasks("grunt-shell");
 
   grunt.registerTask("default", ["jshint"]);
-  grunt.registerTask("git", ["shell:gitAdd:gitcommit:testing grunt:gitPush"]);
+  grunt.registerTask("git", ["shell:add:commit:push"]);
+  grunt.registerTask("gitadd", ["shell:add"]);
 };
