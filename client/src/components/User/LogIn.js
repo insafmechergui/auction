0cbd779d5e5774d642525040f5b22fc27edd7102
Mohamed-Dhia <dhiaweslati16@gmail.com>
2020-01-11 11:59:36 +0100

    commited by grunt gang  Fixed_bug_with_the_logIN:not_refreshing

--- client/src/components/User/LogIn.js
+++ client/src/components/User/LogIn.js
@@ -1,6 +1,6 @@
 import React from "react";
 import logInServices from "../../services/logInservice.js";
-import { Button, Form, Modal } from 'react-bootstrap';
+import { Button, Form, Modal } from "react-bootstrap";
 class LogIn extends React.Component {
   constructor(props) {
     super(props);
@@ -14,7 +14,7 @@ class LogIn extends React.Component {
 
   logIn() {
     logInServices.checkForLogIn(this.state).then(res => {
-      this.props.changeUserName(res.data.user.name);
+      this.props.changeUserName(res.data.user.id, res.data.user.name);
       window.localStorage.setItem("token", res.data.token);
     });
   }
@@ -26,40 +26,26 @@ class LogIn extends React.Component {
 
   handleChange(e) {
     this.setState({ [e.target.name]: e.target.value });
-
   }
 
   render() {
     return (
-      // <form onSubmit={this.handleSubmit}>
-      //   <label for="email">
-      //     email
-      //     <input
-      //       type="text"
-      //       name="email"
-      //       value={this.state.email}
-      //       onChange={this.handleChange}
-      //     />
-      //   </label>
-      //   <br />
-      //   <label for="password">
-      //     password
-      //     <input
-      //       type="password"
-      //       name="password"
-      //       value={this.state.password}
-      //       onChange={this.handleChange}
-      //     />
-      //   </label>
-      //   <br />
-      //   <input type="submit" value="Submit" />
-      // </form>
-
-      <Modal className='signup' show={this.props.showModal} onHide={() => { this.props.onHide() }}>
-        <Form onSubmit={this.handleSubmit} >
+      <Modal
+        className="signup"
+        show={this.props.showModal}
+        onHide={() => {
+          this.props.onHide();
+        }}
+      >
+        <Form onSubmit={this.handleSubmit}>
           <Form.Group controlId="formBasicEmail">
             <Form.Label>Email</Form.Label>
-            <Form.Control name="email" type="email" placeholder="Enter email" onChange={this.handleChange} />
+            <Form.Control
+              name="email"
+              type="email"
+              placeholder="Enter email"
+              onChange={this.handleChange}
+            />
             <Form.Text className="text-muted">
               We'll never share your email with anyone else.
             </Form.Text>
@@ -67,18 +53,23 @@ class LogIn extends React.Component {
 
           <Form.Group controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control name="password" type="password" placeholder="Password" onChange={this.handleChange} />
+            <Form.Control
+              name="password"
+              type="password"
+              placeholder="Password"
+              onChange={this.handleChange}
+            />
           </Form.Group>
           <Form.Group controlId="formBasicCheckbox">
             <Form.Check type="checkbox" label="Check me out" />
           </Form.Group>
-          <Form.Group className='text-center'>
+          <Form.Group className="text-center">
             <Button variant="primary" type="submit">
               Login
-          </Button></Form.Group>
+            </Button>
+          </Form.Group>
         </Form>
       </Modal>
-
     );
   }
 }
