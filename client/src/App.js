63243e33e77fe70e6037d370637c542db59637aa
aminaouini <aminaouini565@gmail.com>
2020-01-12 12:40:57 +0100

    conflict solved with dhia

--- client/src/App.js
+++ client/src/App.js
@@ -103,6 +103,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
+        <Admin userInfo={this.state.userInfo} />
         <Router>
           <SignUp
             showModal={this.state.showModalSignUp}
@@ -150,23 +151,23 @@ class App extends React.Component {
                     </Nav.Link>
                   </Nav>
                 ) : (
-                  <Nav>
-                    <Nav.Link
-                      onClick={() => {
-                        this.hundleSignOut();
-                      }}
-                    >
-                      SignOut
+                    <Nav>
+                      <Nav.Link
+                        onClick={() => {
+                          this.hundleSignOut();
+                        }}
+                      >
+                        SignOut
                     </Nav.Link>
-                    <Nav.Link
-                      onClick={() => {
-                        this.handleShow("SignUp");
-                      }}
-                    >
-                      {this.state.userInfo.name}
-                    </Nav.Link>
-                  </Nav>
-                )}
+                      <Nav.Link
+                        onClick={() => {
+                          this.handleShow("SignUp");
+                        }}
+                      >
+                        {this.state.userInfo.name}
+                      </Nav.Link>
+                    </Nav>
+                  )}
                 <Form inline>
                   <FormControl
                     type="text"
--- client/src/components/admin/Admin.js
+++ client/src/components/admin/Admin.js
@@ -1,7 +1,9 @@
+
 import React from 'react';
 import { Button, Form, InputGroup } from 'react-bootstrap';
 import AddProduct from '../Product/addProduct.js'
 import serviceProduct from '../../services/productService.js';
+import adminServices from './../../services/adminServices.js'
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 
 class Admin extends React.Component {
@@ -12,8 +14,12 @@ class Admin extends React.Component {
             products: []
         }
     }
-
+    checkauth(id) {
+        //call function from the admin service to check if can enter this page
+        adminServices.checkIfAdmin(id);
+    }
     componentDidMount() {
+        this.checkauth(this.props.userInfo.id);
         serviceProduct.getAll()
             .then(res => { this.setState({ products: res.data }) }
             ).then(res => { console.log('hello', this.state.products) }
@@ -33,10 +39,9 @@ class Admin extends React.Component {
                         <TableHeaderColumn dataField='initial_date'>Start Date</TableHeaderColumn>
                     </BootstrapTable>
                 </div>
-            </React.Fragment>
-
-        )
+            </React.Fragment>)
     }
+
 }
 
 export default Admin;
