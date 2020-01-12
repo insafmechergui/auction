f1a2292ad5b205ee95738b50a0f0c792f0607f9a
aminaouini <aminaouini565@gmail.com>
2020-01-12 12:31:27 +0100

    change api name

--- client/src/components/admin/Admin.js
+++ client/src/components/admin/Admin.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Button, Form, InputGroup } from 'react-bootstrap';
 import AddProduct from '../Product/addProduct.js'
-// import { getAll } from '..\..\services\productService.js'
+import serviceProduct from '../../services/productService.js';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 
 class Admin extends React.Component {
     constructor(props) {
@@ -12,9 +13,12 @@ class Admin extends React.Component {
         }
     }
 
-    // componentDidMount() {
-    //     getA
-    // }
+    componentDidMount() {
+        serviceProduct.getAll()
+            .then(res => { this.setState({ products: res.data }) }
+            ).then(res => { console.log('hello', this.state.products) }
+            )
+    }
 
     render() {
         return (
@@ -23,7 +27,11 @@ class Admin extends React.Component {
                     <AddProduct></AddProduct>
                 </div>
                 <div id="productTable">
-
+                    <BootstrapTable data={this.state.products} >
+                        <TableHeaderColumn isKey dataField='_id'>ID</TableHeaderColumn>
+                        <TableHeaderColumn dataField='name'>Name </TableHeaderColumn>
+                        <TableHeaderColumn dataField='initial_date'>Start Date</TableHeaderColumn>
+                    </BootstrapTable>
                 </div>
             </React.Fragment>
 
