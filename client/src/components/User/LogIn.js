764f43c74603f9d57916dfe2270974ba012678a8
Mohamed-Dhia <dhiaweslati16@gmail.com>
2020-01-12 15:21:39 +0100

    fixed bugs with login

--- client/src/components/User/LogIn.js
+++ client/src/components/User/LogIn.js
@@ -1,25 +1,35 @@
 import React from "react";
 import logInServices from "../../services/logInservice.js";
-import { Button, Form, Modal } from "react-bootstrap";
+import { Button, Form, Modal, Alert } from "react-bootstrap";
 class LogIn extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       email: "",
-      password: ""
+      password: "",
+      alert: { state: false, text: "", variant: "" }
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   logIn() {
-    logInServices.checkForLogIn(this.state).then(res => {
-      if (res) {
-        this.props.changeUserName(res.data.user._id, res.data.user.name);
-        window.localStorage.setItem("token", res.data.token);
-        this.props.handleShow("Login");
-      }
-    });
+    logInServices
+      .checkForLogIn(this.state)
+      .then(res => {
+        // send the user the res.data
+        if (res.status === 404) {
+          this.setState({
+            alert: { state: true, text: res.data, variant: "danger" }
+          });
+        }
+        if (res.status === 200) {
+          this.props.changeUserName(res.data.user._id, res.data.user.name);
+          window.localStorage.setItem("token", res.data.token);
+          this.props.handleShow("Login");
+        }
+      })
+      .catch(err => console.log(err));
   }
 
   handleSubmit(e) {
@@ -41,6 +51,13 @@ class LogIn extends React.Component {
         }}
       >
         <Form onSubmit={this.handleSubmit}>
+          <Alert
+            variant={this.state.alert.variant}
+            show={this.state.alert.state}
+            dismissible
+          >
+            {this.state.alert.text}
+          </Alert>
           <Form.Group controlId="formBasicEmail">
             <Form.Label>Email</Form.Label>
             <Form.Control
@@ -53,7 +70,6 @@ class LogIn extends React.Component {
               We'll never share your email with anyone else.
             </Form.Text>
           </Form.Group>
-
           <Form.Group controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
             <Form.Control
