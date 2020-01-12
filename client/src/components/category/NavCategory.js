a9917535b166fe62acf9312e8785a02d8c33e8be
mfmehdi <mfmehdi2@gmail.com>
2020-01-13 00:02:03 +0100

    fix bug link nav , search

--- client/src/components/category/NavCategory.js
+++ client/src/components/category/NavCategory.js
@@ -58,29 +58,59 @@ class NavCategory extends React.Component {
       // </div>
 
 
-      <div className='navcategory'>
-        {/* navbar category change the sticky top */}
-        <Navbar sticky="top">
-          <Navbar.Brand>Category</Navbar.Brand>
-          <Nav className="mr-auto ">
-            <div className='link' >
-              {this.state.categoryList.map(cat => {
-                return (
-                  <div >
-                    <Link className='link'
-                      to="/"
-                      onClick={e => {
-                        this.handleClickCategory(cat.name);
-                      }}
-                    >
-                      {cat.name}
-                    </Link>
-                  </div>
-                );
-              })}</div>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand >Categories</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+
+            {this.state.categoryList.map(cat => {
+              return (
+                <Nav.Link>
+                  <Link className="text-decoration-none text-white"
+                    to="/"
+                    onClick={e => {
+                      this.handleClickCategory(cat.name);
+                    }}
+                  >
+                    {cat.name}
+                  </Link></Nav.Link>
+
+              );
+            })}
           </Nav>
-        </Navbar>
-      </div>
+          {/* <Nav>
+            <Nav.Link href="#deets">More deets</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Dank memes
+      </Nav.Link>
+          </Nav> */}
+        </Navbar.Collapse>
+      </Navbar>
+
+      // <div className='navcategory'>
+      //   {/* navbar category change the sticky top */}
+      //   <Navbar sticky="top">
+      //     <Navbar.Brand>Category</Navbar.Brand>
+      //     <Nav className="mr-auto ">
+      //       <div className='link' >
+      //         {this.state.categoryList.map(cat => {
+      //           return (
+      //             <div >
+      //               <Link className='link'
+      //                 to="/"
+      //                 onClick={e => {
+      //                   this.handleClickCategory(cat.name);
+      //                 }}
+      //               >
+      //                 {cat.name}
+      //               </Link>
+      //             </div>
+      //           );
+      //         })}</div>
+      //     </Nav>
+      //   </Navbar>
+      // </div>
     );
   }
 }
--- routes/ProductRoute.js
+++ routes/ProductRoute.js
@@ -43,6 +43,7 @@ module.exports = app => {
 
   app.post("/api/addp", (req, res) => {
     console.log(req.body);
+    //.body.images = req.body.images.split(',')
     Product.create(req.body, (err, result) => {
       if (err) {
         res.status(400).send(err);
@@ -144,7 +145,8 @@ module.exports = app => {
 
   app.get("/api/productsearch", (req, res) => {
     Products.searchFilter(req.query.descreption, (err, data) => {
-      if (err) res.status(404).send("not found");
+      if (err)
+        res.status(404).send("not found");
       res.send(data);
     });
   });
