8e63e611c2d242e714a831e3b0150dbab4976685
insafmechergui <insaf_mechergui@outlook.com>
2020-01-13 08:08:39 +0100

    merging

--- routes/CategoryRoute.js
+++ routes/CategoryRoute.js
@@ -3,51 +3,49 @@ const Category = require("../Database/Category ");
 module.exports = app => {
   app.post("/api/AddCategory", (req, res) => {
     console.log(req.body);
-    
 
-    app.post('/api/AddCategory', (req, res) => {
-
-        Category.createCategory(req.body, (err, data) => {
-            if (err) {
-                res.status(404).send(err)
-                console.log('duplicate key')
-            } else {
-                res.status(200).send(data)
-            }
-        })
-    })
-
-  app.delete("/api/deleteCategory", (req, res) => {
-    Category.deleteCategory(req.body.name, (err, result) => {
-      if (result) {
-        res.status(200).send("true");
-      } else {
-        res.status(404).send("false");
-      }
+    app.post("/api/AddCategory", (req, res) => {
+      Category.createCategory(req.body, (err, data) => {
+        if (err) {
+          res.status(404).send(err);
+          console.log("duplicate key");
+        } else {
+          res.status(200).send(data);
+        }
+      });
     });
-  });
 
-  app.get("/api/getProducts", (req, res) => {
-    //get productbycategory
-    console.log(req.query.category);
-    Category.getAllProductByCategory(req.query.category, (err, data) => {
-      if (err) {
-        res.status(401).send(err);
-      } else {
-        res.status(200).send(data);
-      }
+    app.delete("/api/deleteCategory", (req, res) => {
+      Category.deleteCategory(req.body.name, (err, result) => {
+        if (result) {
+          res.status(200).send("true");
+        } else {
+          res.status(404).send("false");
+        }
+      });
     });
-  });
 
+    app.get("/api/getProducts", (req, res) => {
+      //get productbycategory
+      console.log(req.query.category);
+      Category.getAllProductByCategory(req.query.category, (err, data) => {
+        if (err) {
+          res.status(401).send(err);
+        } else {
+          res.status(200).send(data);
+        }
+      });
+    });
 
-  app.get("/api/categories", (req, res) => {
-    Category.getAll((err, data) => {
-      if (err) {
-        res.status(404).send(err);
-      } else {
-        res.status(200).send(data);
-        res.end();
-      }
+    app.get("/api/categories", (req, res) => {
+      Category.getAll((err, data) => {
+        if (err) {
+          res.status(404).send(err);
+        } else {
+          res.status(200).send(data);
+          res.end();
+        }
+      });
     });
   });
 };
